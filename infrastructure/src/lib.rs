318b1c87b75dad1ee7d2dd0069b047d02e778cbb
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-01-23 14:57:48 -0500

    feat: changed the size of the connection pool

--- infrastructure/src/lib.rs
+++ infrastructure/src/lib.rs
@@ -1,34 +1,50 @@
 use diesel::mysql::MysqlConnection;
-use dotenvy::dotenv;
+use diesel::r2d2;
 use std::env;
+use std::ops::{Deref, DerefMut};
 
 use shared::parameters::Series;
 
-pub struct Connection {
-    pool: MysqlConnection,
+pub struct ConnectionPool {
+    f1db_pool: DBPool,
 }
 
-impl Connection {
-    pub fn pool_from_series(&mut self, series: Series) -> &mut MysqlConnection {
+impl ConnectionPool {
+    pub fn try_new() -> Result<Self, ()> {
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+        println!("Connecting to {}", database_url);
+
+        let manager = r2d2::ConnectionManager::<MysqlConnection>::new(database_url);
+        let pool = r2d2::Pool::builder()
+            .max_size(20)
+            .build(manager)
+            .expect("Failed to create pool.");
+
+        Ok(Self { f1db_pool: DBPool(pool) } )
+    }
+
+    pub fn from_series(&self, series: Series) -> &DBPool {
         match series {
-            Series::F1 => &mut self.pool,
-            Series::F2 => &mut self.pool,
+            Series::F1 => &self.f1db_pool,
+            Series::F2 => unimplemented!(),
         }
     }
 }
 
-impl Default for Connection {
-    fn default() -> Self {
-        use diesel::Connection;
 
-        dotenv().ok();
+type InnerDBPool = r2d2::Pool<r2d2::ConnectionManager<MysqlConnection>>;
+pub struct DBPool(InnerDBPool);
 
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
-        println!("Connecting to {}", database_url);
+impl Deref for DBPool {
+    type Target = InnerDBPool;
 
-        Self {
-            pool: MysqlConnection::establish(&database_url)
-                .expect("Failed to connect to database."),
-        }
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for DBPool {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
