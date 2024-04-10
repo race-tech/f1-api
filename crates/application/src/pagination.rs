cf59f85697d0e33e43323f4280103adadb45d58f
thibault-cne <thibault.chene23@gmail.com>
2024-04-10 04:05:20 -0400

    feat: pagination in one query

--- crates/application/src/pagination.rs
+++ crates/application/src/pagination.rs
@@ -1,6 +1,8 @@
 use mysql::prelude::*;
 use mysql::*;
-use sea_query::{Alias, Asterisk, Expr, Func, MysqlQueryBuilder, Query, SelectStatement};
+use sea_query::{
+    Alias, Asterisk, Expr, MysqlQueryBuilder, Query, SelectStatement, WindowStatement,
+};
 
 use infrastructure::Connection;
 use shared::models::Pagination;
@@ -42,15 +44,21 @@ impl Paginated {
     where
         U: FromRow,
     {
-        let query = self
-            .query
-            .clone()
+        let query = dbg!(Query::select()
+            .column(Asterisk)
+            .expr_window_as(
+                Expr::cust("COUNT(*)"),
+                WindowStatement::new(),
+                Alias::new("pagination_count_total")
+            )
+            .from_subquery(self.query, Alias::new("t"))
             .limit(self.per_page)
             .offset(self.offset)
-            .to_string(MysqlQueryBuilder);
+            .to_string(MysqlQueryBuilder));
 
-        let records: Vec<U> = conn.query(query).unwrap();
-        let total = self.num_items(conn);
+        let res: Vec<PaginationResult<U>> = conn.query(query).unwrap();
+        let total = res.first().map(|r| r.total).unwrap_or(0);
+        let records = res.into_iter().map(|r| r.data).collect();
         let total_pages = (total as f64 / self.per_page as f64).ceil() as u64;
 
         let pagination = Pagination {
@@ -62,25 +70,52 @@ impl Paginated {
 
         (records, pagination)
     }
-
-    fn num_items(&self, conn: &mut Connection) -> u64 {
-        let query = Query::select()
-            .expr(Expr::cust("COUNT(*) AS num_items"))
-            .from_subquery(
-                self.query
-                    .clone()
-                    .reset_limit()
-                    .reset_offset()
-                    .clear_order_by()
-                    .to_owned(),
-                Alias::new("t"),
-            )
-            .to_string(MysqlQueryBuilder);
-
-        *conn.query(query).unwrap().first().unwrap()
-    }
 }
 
 fn offset(page: u64, per_page: u64) -> u64 {
     (page.saturating_sub(1)) * per_page
+}
+
+struct PaginationResult<U> {
+    data: U,
+    total: u64,
+}
+
+impl<U> FromRow for PaginationResult<U>
+where
+    U: FromRow,
+{
+    fn from_row(mut row: Row) -> Self
+    where
+        Self: Sized,
+    {
+        let total = row.take("pagination_count_total");
+        assert!(total.is_some(), "error while querying pagination count");
+
+        let data = U::from_row(row);
+
+        PaginationResult {
+            data,
+            total: total.unwrap(),
+        }
+    }
+
+    fn from_row_opt(mut row: Row) -> std::prelude::v1::Result<Self, FromRowError>
+    where
+        Self: Sized,
+    {
+        let total = row.take_opt("pagination_count_total");
+        assert!(total.is_some(), "error while querying pagination count");
+        assert!(
+            total.as_ref().unwrap().is_ok(),
+            "error while parsing pagination count"
+        );
+
+        let data = U::from_row_opt(row)?;
+
+        Ok(PaginationResult {
+            data,
+            total: total.unwrap().unwrap(),
+        })
+    }
 }
