78776a9a05c744b410861c9eb68020d3966d8c7b
Thibault Cheneviere <thibault.chene23@gmail.com>
2026-01-22 12:02:56 +0100

    feat: cleanup dependancies

--- crates/error/src/lib.rs
+++ crates/error/src/lib.rs
@@ -38,8 +38,6 @@ pub enum ErrorKind {
     FromUtf8,
 
     // External error kinds
-    R2D2,
-    Mysql,
     Figment,
     Serde,
     Axum,
@@ -47,8 +45,6 @@ pub enum ErrorKind {
     TimeFormat,
 }
 
-macros::error_from!(R2D2 => r2d2::Error);
-macros::error_from!(Mysql => mysql::Error);
 macros::error_from!(Figment => figment::Error);
 macros::error_from!(Serde => serde_json::Error);
 macros::error_from!(Axum => axum::Error);
@@ -82,8 +78,6 @@ impl std::fmt::Display for ErrorKind {
             ParseInt => write!(f, "can't parse int"),
             FromUtf8 => write!(f, "invalid utf8 string"),
 
-            R2D2 => write!(f, "r2d2 error"),
-            Mysql => write!(f, "mysql error"),
             Figment => write!(f, "figment error"),
             Serde => write!(f, "serde error"),
             Axum => write!(f, "axum error"),
@@ -113,8 +107,6 @@ impl Serialize for ErrorKind {
             ParseInt => s.serialize_unit_variant("ErrorKind", 8, "ParseInt"),
             FromUtf8 => s.serialize_unit_variant("ErrorKind", 9, "FromUtf8"),
 
-            R2D2 => s.serialize_unit_variant("ErrorKind", 10, "R2D2"),
-            Mysql => s.serialize_unit_variant("ErrorKind", 11, "Mysql"),
             Figment => s.serialize_unit_variant("ErrorKind", 13, "Figment"),
             Serde => s.serialize_unit_variant("ErrorKind", 14, "Serde"),
             Axum => s.serialize_unit_variant("ErrorKind", 15, "Axum"),
@@ -143,8 +135,6 @@ impl From<ErrorKind> for StatusCode {
             ParseInt => Self::INTERNAL_SERVER_ERROR,
             FromUtf8 => Self::INTERNAL_SERVER_ERROR,
 
-            R2D2 => Self::INTERNAL_SERVER_ERROR,
-            Mysql => Self::INTERNAL_SERVER_ERROR,
             Figment => Self::INTERNAL_SERVER_ERROR,
             Serde => Self::INTERNAL_SERVER_ERROR,
             Axum => Self::INTERNAL_SERVER_ERROR,
