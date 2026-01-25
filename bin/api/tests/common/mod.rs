1e4150b55da94ae95724f2c7003999d49e8f3367
Thibault Cheneviere <thibault.chene23@gmail.com>
2026-01-25 19:07:20 +0100

    fix: clippy on test module

--- bin/api/tests/common/mod.rs
+++ bin/api/tests/common/mod.rs
@@ -3,12 +3,14 @@ use async_graphql::dynamic::Schema;
 #[path = "../../src/query_root.rs"]
 mod query_root;
 
+#[allow(dead_code)]
 pub struct Test<'a> {
     query: &'a str,
     expected: Option<serde_json::Value>,
     schema: Schema,
 }
 
+#[allow(dead_code)]
 impl<'a> Test<'a> {
     pub async fn new(query: &'a str) -> Test<'a> {
         Test {
@@ -77,7 +79,6 @@ async fn setup() -> Schema {
     let pool = infrastructure::create_database_conn_pool(&config)
         .await
         .expect("unable to create database pool");
-    let schema = query_root::schema(pool.into(), None, None).unwrap();
 
-    schema
+    query_root::schema(pool.into(), None, None).unwrap()
 }
